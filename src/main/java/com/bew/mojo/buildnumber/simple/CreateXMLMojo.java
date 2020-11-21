315c2be09de68f9b7d67ed8d7d2a8a5fb15e0181
Bradley Willcott <bw.opensource@yahoo.com>
2020-11-21 19:23:27 +0800

    Added setup/configuration info to README.md and CreateXMLMojo class Javadoc comment.  Also added new profile naming and additional profile to "pom.xml". Signed-off-by:Bradley Willcott <bw.opensource@yahoo.com>

--- src/main/java/com/bew/mojo/buildnumber/simple/CreateXMLMojo.java
+++ src/main/java/com/bew/mojo/buildnumber/simple/CreateXMLMojo.java
@@ -34,63 +34,58 @@ import static java.util.regex.Pattern.MULTILINE;
 import static org.apache.maven.plugins.annotations.LifecyclePhase.VALIDATE;
 
 /**
- * Creates/Increments a build number for the project, and updates the "project.version" in the "pom.xml"
- * file for the project.
- * <p>
- * You must set the property: major.minor.version
- * </p><p>
- * For example, if your project was at version 1.0-SNAPSHOT,<br>
+ * The plugin Creates/Increments a build number for the project, and updates the "&lt;project.version&gt;"
+ * in the "<i>pom.xml</i>" file for the project. Simple. So is setting up.
+ * <h2>Setup</h2>
+ * You must set the property: &lt;major.minor.version&gt;<br>
+ * For example, if your project was at version "1.0-SNAPSHOT",<br>
  * then you would set the following:
- * </p>
  * <pre><code>
- *
  *&lt;project ...&gt;
  *...
- *    &lt;properties&gt;
- *        &lt;major.minor.version&gt;1.0&lt;/major.minor.version&gt;
- *        ...
- *    &lt;/properties&gt;
+ *   &lt;properties&gt;
+ *       &lt;major.minor.version&gt;1.0&lt;/major.minor.version&gt;
+ *       ...
+ *   &lt;/properties&gt;
  *...
- *    &lt;profiles&gt;
+ *   &lt;profiles&gt;
+ *       &lt;profile&gt;
+ *           &lt;!-- Increment {buildNumber} and update the 'pom.xml' file --&gt;
+ *           &lt;id&gt;increment-build&lt;/id&gt;
+ *           &lt;build&gt;
+ *               &lt;plugins&gt;
+ *                   &lt;plugin&gt;
+ *                       &lt;groupId&gt;com.bew.mojo&lt;/groupId&gt;
+ *                       &lt;artifactId&gt;simple-buildnumber-maven-plugin&lt;/artifactId&gt;
+ *                       &lt;version&gt;1.0&lt;/version&gt;
+ *                       &lt;executions&gt;
+ *                           &lt;execution&gt;
+ *                               &lt;id&gt;Increment Build&lt;/id&gt;
+ *                               &lt;goals&gt;
+ *                                   &lt;goal&gt;create-xml&lt;/goal&gt;
+ *                               &lt;/goals&gt;
+ *                           &lt;/execution&gt;
+ *                       &lt;/executions&gt;
+ *                   &lt;/plugin&gt;
+ *               &lt;/plugins&gt;
+ *           &lt;/build&gt;
+ *       &lt;/profile&gt;
  *        &lt;profile&gt;
- *            &lt;!-- Increment {buildNumber} and update the 'pom.xml' file --&gt;
- *            &lt;id&gt;dev&lt;/id&gt;
+ *            &lt;id&gt;increment-release&lt;/id&gt;
  *            &lt;build&gt;
  *                &lt;plugins&gt;
+ *                    &lt;!-- Increment {buildNumber}, remove '-SNAPSHOT' and update the 'pom.xml' file --&gt;
  *                    &lt;plugin&gt;
  *                        &lt;groupId&gt;com.bew.mojo&lt;/groupId&gt;
  *                        &lt;artifactId&gt;simple-buildnumber-maven-plugin&lt;/artifactId&gt;
- *                        &lt;version&gt;1.0&lt;/version&gt;
  *                        &lt;executions&gt;
  *                            &lt;execution&gt;
- *                                &lt;id&gt;Update&lt;/id&gt;
- *                                &lt;goals&gt;
- *                                    &lt;goal&gt;create-xml&lt;/goal&gt;
- *                                &lt;/goals&gt;
- *                            &lt;/execution&gt;
- *                        &lt;/executions&gt;
- *                    &lt;/plugin&gt;
- *                &lt;/plugins&gt;
- *            &lt;/build&gt;
- *        &lt;/profile&gt;
- *        &lt;profile&gt;
- *            &lt;!-- Keep current {buildNumber}, remove '-SNAPSHOT' and update the 'pom.xml' file --&gt;
- *            &lt;id&gt;release&lt;/id&gt;
- *            &lt;build&gt;
- *                &lt;plugins&gt;
- *                    &lt;plugin&gt;
- *                        &lt;groupId&gt;com.bew.mojo&lt;/groupId&gt;
- *                        &lt;artifactId&gt;simple-buildnumber-maven-plugin&lt;/artifactId&gt;
- *                        &lt;version&gt;1.0&lt;/version&gt;
- *                        &lt;executions&gt;
- *                            &lt;execution&gt;
- *                                &lt;id&gt;Release&lt;/id&gt;
+ *                                &lt;id&gt;Increment Release&lt;/id&gt;
  *                                &lt;goals&gt;
  *                                    &lt;goal&gt;create-xml&lt;/goal&gt;
  *                                &lt;/goals&gt;
  *                                &lt;configuration&gt;
  *                                    &lt;release&gt;true&lt;/release&gt;
- *                                    &lt;keepNumber&gt;true&lt;/keepNumber&gt;
  *                                &lt;/configuration&gt;
  *                            &lt;/execution&gt;
  *                        &lt;/executions&gt;
@@ -98,13 +93,99 @@ import static org.apache.maven.plugins.annotations.LifecyclePhase.VALIDATE;
  *                &lt;/plugins&gt;
  *            &lt;/build&gt;
  *        &lt;/profile&gt;
- *        ...
- *    &lt;/profiles&gt;
+ *       &lt;profile&gt;
+ *           &lt;!-- Keep current {buildNumber}, remove '-SNAPSHOT' and update the 'pom.xml' file --&gt;
+ *           &lt;id&gt;release&lt;/id&gt;
+ *           &lt;build&gt;
+ *               &lt;plugins&gt;
+ *                   &lt;plugin&gt;
+ *                       &lt;groupId&gt;com.bew.mojo&lt;/groupId&gt;
+ *                       &lt;artifactId&gt;simple-buildnumber-maven-plugin&lt;/artifactId&gt;
+ *                       &lt;version&gt;1.0&lt;/version&gt;
+ *                       &lt;executions&gt;
+ *                           &lt;execution&gt;
+ *                               &lt;id&gt;Release&lt;/id&gt;
+ *                               &lt;goals&gt;
+ *                                   &lt;goal&gt;create-xml&lt;/goal&gt;
+ *                               &lt;/goals&gt;
+ *                               &lt;configuration&gt;
+ *                                   &lt;release&gt;true&lt;/release&gt;
+ *                                   &lt;keepNumber&gt;true&lt;/keepNumber&gt;
+ *                               &lt;/configuration&gt;
+ *                           &lt;/execution&gt;
+ *                       &lt;/executions&gt;
+ *                   &lt;/plugin&gt;
+ *               &lt;/plugins&gt;
+ *           &lt;/build&gt;
+ *       &lt;/profile&gt;
+ *       ...
+ *   &lt;/profiles&gt;
  *...
  *&lt;/project&gt;
- *</code></pre>
+ * </code></pre>
+ * (<i>By the way. You could be really lazy, and just copy the relevant sections from above and paste them</i>
+ * <i>directly into your <code>pom.xml</code> file.</i>)
+ * <p>
+ * When you run a profile other than one of the above, then nothing happens to the <code>&lt;project.version&gt;</code> setting.
+ * So, to increment the build number, select the profile: <code>increment-build</code>.
+ * <p>
+ * Then go back to your normal development profile.
+ * <p>
+ * When you are ready to release a version:
+ * <ul>
+ * <li>with a new build number, either:
+ * <ul>
+ * <li>increment the build number (as above) then run the profile: <code>release</code>, or</li>
+ * <li>run the profile: <code>increment-release</code>, which does both in one shot.</li></ul></li>
+ * <li>with the current build number:
+ * <ul>
+ * <li>run the profile: <code>release</code>, only.</li></ul></li>
+ * </ul>
+ * Then if necessary, run your normal release profile.
+ * <p>
+ * Of course, you can merge these profiles into your own as you see fit. But remember, the <code>increment-build</code>
+ * and <code>increment-release</code> profiles' version of the plugin <b>will</b> increment the number <b>every time</b>
+ * it is run. So be aware of possible run-away build numbers.
+ * <h3>Properties</h3>
+ * <h4>&lt;major.minor.version&gt;</h4>
+ * {<b>Required</b>}<br>
+ * The project's base version number as: _major.minor_ (eg: 1.0). The text you place here is __not__
+ * validated by this plugin. However, incorrectly setting this might affect other parts of the build
+ * system/IDE environment. Also, if you want to use a longer version number, such as: "1.1.2.3.4", etc.,
+ * then again this plugin won't check.
+ * <p>
+ * The build number (and if not <i>release</i>: "-SNAPSHOT") will be appended to this string.
+ * For example: "1.0.1-SNAPSHOT", "1.1.2.3.4.1-SNAPSHOT".
+ * <h4>&lt;simple.buildNumber.indentSpaces&gt;</h4>
+ * Alternative to &lt;indentSpaces&gt;. Use either one, not both.
+ * <h4>&lt;simple.buildNumber.propertiesFilename&gt;</h4>
+ * {default: "<code>${project.basedir}/buildNumber.properties</code>"}<br>
+ * The properties file to be created/used.
+ * <h4>&lt;simple.buildNumber.propertyName&gt;</h4>
+ * {default: "<code>buildNumber</code>"}<br>
+ * Sets the build number property name. This is used in the properties file, and is available to other
+ * plugins during the current run. (Access by other plugins has <b>not</b> been tested)
+ * <h3>Configuration</h3>
+ * The following settings are available for the goal: <code>create-xml</code>.
+ * <h4>&lt;keepNumber&gt;</h4>
+ * {default: <code>false</code>}<br>
+ * Set this to "true" to just keep using the current number in the external properties file.
+ * Incrementing of the number will be disabled for this run.
+ * <h4>&lt;indentSpaces&gt;</h4>
+ * {default: <code>4</code>}<br>
+ * The number of spaces used to indent text. This important, as it is used to find the &lt;project.version&gt;
+ * setting in the <code>pom.xml</code> file.
+ * <h4>&lt;release&gt;</h4>
+ * {default: <code>false</code>}<br>
+ * If not set to <i>true</i>, then "-SNAPSHOT" will be appended to the value to be set in &lt;project.version&gt;
+ * in the <code>pom.xml</code> file.
+ * <h4>&lt;skip&gt;</h4>
+ * {default: <code>false</code>}<br>
+ * Whether or not to skip this execution. A little superfluous when you set things up as above. This may be
+ * removed in a later release.
  *
  * @author  <a href="mailto:bw.opensource@yahoo.com">Bradley Willcott</a>
+ *
  * @since 1.0
  * @version 1.0.9
  */
@@ -125,7 +206,7 @@ public class CreateXMLMojo extends AbstractMojo {
     private static final Logger log = LogManager.getLogger();
 
     /**
-     * You can rename the buildNumber property name to another property name if desired.
+     * You can rename the build number property name to another name if desired.
      */
     @Parameter(property = "simple.buildNumber.propertyName", defaultValue = BUILDNUMBER, readonly = true)
     private String buildNumberPropertyName;
@@ -178,7 +259,7 @@ public class CreateXMLMojo extends AbstractMojo {
     private File pomFile;
 
     /**
-     * Properties file to be created.
+     * The properties file to be created/used.
      */
     @Parameter(property = "simple.buildNumber.propertiesFilename",
                defaultValue = "${project.basedir}/buildNumber.properties", readonly = true)
@@ -193,7 +274,7 @@ public class CreateXMLMojo extends AbstractMojo {
 
     // ////////////////////////////////////// internal variables ///////////////////////////////////
     /**
-     * Whether to skip this execution.
+     * Whether or not to skip this execution.
      */
     @Parameter(defaultValue = "false")
     private boolean skip;
@@ -282,7 +363,7 @@ public class CreateXMLMojo extends AbstractMojo {
             String strBuildNumber = null;
 
             // get the number for the buildNumber specified
-            try (FileInputStream inputStream = new FileInputStream(propertiesFileLocation))
+            try ( FileInputStream inputStream = new FileInputStream(propertiesFileLocation))
             {
                 properties.load(inputStream);
 
@@ -323,7 +404,7 @@ public class CreateXMLMojo extends AbstractMojo {
                 logTrace("Storing properties: {}", propertiesFileLocation);
                 logDebug(properties.toString());
 
-                try (FileOutputStream outputStream = new FileOutputStream(propertiesFileLocation))
+                try ( FileOutputStream outputStream = new FileOutputStream(propertiesFileLocation))
                 {
                     properties.store(outputStream, "simple-buildnumber-maven-plugin properties file");
                 } catch (IOException ex)
@@ -473,7 +554,7 @@ public class CreateXMLMojo extends AbstractMojo {
 
         logTrace("About to read in pom file: {}", pomFile);
 
-        try (BufferedReader in = new BufferedReader(new FileReader(pomFile)))
+        try ( BufferedReader in = new BufferedReader(new FileReader(pomFile)))
         {
             while ((input = in.readLine()) != null)
             {
@@ -503,7 +584,7 @@ public class CreateXMLMojo extends AbstractMojo {
         m.appendTail(sb);
         logDebug("sb:\n" + sb);
 
-        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(pomFile))))
+        try ( PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(pomFile))))
         {
             out.print(sb);
         } catch (IOException ex)
