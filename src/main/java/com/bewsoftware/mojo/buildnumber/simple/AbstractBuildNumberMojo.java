85143df22c0db35369ae58b2c26943dc4f527242
Bradley Willcott <bw.opensource@yahoo.com>
2021-01-27 00:04:59 +0800

    Clean-up. Signed-off-by:Bradley Willcott <bw.opensource@yahoo.com>

--- src/main/java/com/bewsoftware/mojo/buildnumber/simple/AbstractBuildNumberMojo.java
+++ src/main/java/com/bewsoftware/mojo/buildnumber/simple/AbstractBuildNumberMojo.java
@@ -20,7 +20,6 @@ import com.bewsoftware.utils.struct.StringReturn;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
